e2f7b506c97be688e0883c2811c109e4fcafe805
Will-Copeland <will@localhost.localdomain>
2020-10-05 19:34:22 -0700

    Add interval and logs

--- src/util/dht22.ts
+++ src/util/dht22.ts
@@ -15,7 +15,10 @@ class DHT22 extends Device {
     super(deviceConfig, transmitterConfig);
 
     this.client.on("message", this.onMessage);
+
+    this.interval = setInterval(this.read, 2000);
   }
+  public interval: NodeJS.Timeout;
 
   onMessage(topic: string, payload: Buffer): void {
     console.log("Incoming message for ", topic, "payload: ", payload);
@@ -24,8 +27,14 @@ class DHT22 extends Device {
   }
 
   read(): void {
+    console.log("Attempting to read data...");
+
     const process = spawn("python", ["./Python/readTemp.py"]);
+    console.log("child process started");
+
     process.stdout.on("data", (data: Buffer) => {
+      console.log("data got");
+
       const str = data.toString();
       const arr = str.split(" ");
       console.log("n", str);
@@ -36,12 +45,13 @@ class DHT22 extends Device {
       });
 
 
+      console.log("attempting to send...", temp, humidity);
+
       this.send("v1/devices/me/telemetry", { temp, humidity }, err => {
         if (err) {
           console.log(`Error sending ${this.deviceConfig.id}`);
         } else {
           console.log("Successfully sent temp and hum update");
-
         }
       })
     });
