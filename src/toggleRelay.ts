6d4be9c770c4ec10ff69fb4c3d6174980a5a43c8
Will-Copeland <will@localhost.localdomain>
2020-10-07 10:32:37 -0700

    Update to absolute path

--- src/toggleRelay.ts
+++ src/toggleRelay.ts
@@ -1,9 +1,9 @@
 import { spawn } from "child_process";
 
 export default (state: 0 | 1, port: number, cb: (relaySetTo: string) => void): void => {
-  console.log(`../Python/${state === 0 ? "relayOff" : "relayOn"}.py ${port}`);
+  console.log(`/home/pi/grovepi-thingsboard/dist/Python/${state === 0 ? "relayOff" : "relayOn"}.py ${port}`);
 
-  const process = spawn("python", [`../Python/${state === 0 ? "relayOff" : "relayOn"}.py ${port}`]);
+  const process = spawn("python", [`/home/pi/grovepi-thingsboard/dist/Python/${state === 0 ? "relayOff" : "relayOn"}.py ${port}`]);
   process.stdout.on("data", data => {
     const output = data.toString();
     console.log("relay set to: ", output);
